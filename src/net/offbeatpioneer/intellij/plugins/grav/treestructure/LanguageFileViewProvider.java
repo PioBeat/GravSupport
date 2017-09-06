59bbe43e9632babfcb2a9d1e7454a658fde0227d
Dominik Grzelak <dominik@offbeat-pioneer.net>
2017-09-06 18:10:46 +0200

    Used SingleRootFileViewProvider instead MultiplePsiFilesPerDocumentFileViewProvider, this fixes #3

--- src/net/offbeatpioneer/intellij/plugins/grav/treestructure/LanguageFileViewProvider.java
+++ src/net/offbeatpioneer/intellij/plugins/grav/treestructure/LanguageFileViewProvider.java
@@ -2,10 +2,7 @@ package net.offbeatpioneer.intellij.plugins.grav.treestructure;
 
 import com.intellij.lang.Language;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.FileViewProvider;
-import com.intellij.psi.FileViewProviderFactory;
-import com.intellij.psi.MultiplePsiFilesPerDocumentFileViewProvider;
-import com.intellij.psi.PsiManager;
+import com.intellij.psi.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.yaml.YAMLLanguage;
 
@@ -13,16 +10,11 @@ public class LanguageFileViewProvider implements FileViewProviderFactory {
     @NotNull
     @Override
     public FileViewProvider createFileViewProvider(@NotNull VirtualFile file, Language language, @NotNull PsiManager manager, boolean eventSystemEnabled) {
-        return new MultiplePsiFilesPerDocumentFileViewProvider(manager, file, eventSystemEnabled) {
+        return new SingleRootFileViewProvider(manager, file, eventSystemEnabled) {
             @NotNull
             @Override
             public Language getBaseLanguage() {
                 return YAMLLanguage.INSTANCE;
-            }
-
-            @Override
-            protected MultiplePsiFilesPerDocumentFileViewProvider cloneInner(VirtualFile fileCopy) {
-                return this;
             }
         };
     }
