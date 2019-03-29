89de73b94b335410cd255d1c1c05163fe2786801
Dominik <dominik.grzelak@web.de>
2019-03-29 18:55:35 +0100

    bugfix project generation

--- src/net/offbeatpioneer/intellij/plugins/grav/module/builder/GravProjectGeneratorUtil.java
+++ src/net/offbeatpioneer/intellij/plugins/grav/module/builder/GravProjectGeneratorUtil.java
@@ -9,15 +9,9 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.ui.MessageType;
-import com.intellij.openapi.ui.popup.Balloon;
-import com.intellij.openapi.ui.popup.JBPopupFactory;
-import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.vfs.VirtualFileEvent;
 import com.intellij.openapi.wm.StatusBar;
 import com.intellij.openapi.wm.WindowManager;
-import com.intellij.ui.awt.RelativePoint;
-import net.offbeatpioneer.intellij.plugins.grav.helper.FileCreateUtil;
 import net.offbeatpioneer.intellij.plugins.grav.helper.NotificationHelper;
 import net.offbeatpioneer.intellij.plugins.grav.module.tasks.InstallDevtoolsPlugin;
 import net.offbeatpioneer.intellij.plugins.grav.module.wizard.CopyFileVisitor;
@@ -26,70 +20,50 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Objects;
 
 import static com.intellij.ide.projectView.actions.MarkRootActionBase.findContentEntry;
 
 public class GravProjectGeneratorUtil {
-    boolean showFailedNotifications = true;
 
     public boolean generateProject(@NotNull Project project, @NotNull VirtualFile src, @NotNull GravProjectSettings settings, @NotNull Module module) {
         StatusBar statusBar = WindowManager.getInstance().getStatusBar(project);
-        ApplicationManager.getApplication().runWriteAction(new Runnable() {
-
-            @Override
-            public void run() {
-                try {
-                    //TODO
-                    ModifiableRootModel model = ModuleRootManager.getInstance(module).getModifiableModel();
-                    if (settings.withSrcDirectory) {
-                        VirtualFile src1 = src.createChildDirectory(this, "src");
-                        findContentEntry(model, src1).addSourceFolder(src1, false);
-
-                        VirtualFile test = src.createChildDirectory(this, "test");
-                        findContentEntry(model, test).addSourceFolder(test, true);
-
-                        model.commit();
-                        test.refresh(false, true);
-                        src1.refresh(false, true);
-                    } else {
-                        findContentEntry(model, src).addSourceFolder(src, false);
-                        model.commit();
-                        src.refresh(false, true);
-                    }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        ApplicationManager.getApplication().runWriteAction(() -> {
+//                try {
+            //TODO
+            ModifiableRootModel model = ModuleRootManager.getInstance(module).getModifiableModel();
+//                    if (settings.withSrcDirectory) {
+//                        VirtualFile src1 = src.createChildDirectory(this, "src");
+//                        findContentEntry(model, src1).addSourceFolder(src1, false);
+//
+//                        VirtualFile test = src.createChildDirectory(this, "test");
+//                        findContentEntry(model, test).addSourceFolder(test, true);
+//
+//                        model.commit();
+//                        test.refresh(false, true);
+//                        src1.refresh(false, true);
+//                    } else {
+            if (Objects.isNull(findContentEntry(model, src))) {
+                return;
             }
+            findContentEntry(model, src).addSourceFolder(src, false);
+            model.commit();
+            src.refresh(false, true);
+//                    }
+
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
         });
 
-        VirtualFile tmp = FileCreateUtil.getParentDirectory(module.getModuleFile(), module.getName());
-        if (tmp == null) return false;
 
-        URI targetPath;
-        try {
-            if (!settings.withSrcDirectory) {
-                targetPath = new URI("file:///" + tmp.getPath());
-            } else {
-                VirtualFile tmp1 = tmp.findFileByRelativePath("src");
-                if (tmp1 != null)
-                    targetPath = new URI("file:///" + tmp1.getPath());
-                else
-                    return false;
-            }
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
+        String targetPath = module.getProject().getBasePath();
+        if (Objects.isNull(targetPath)) {
+            NotificationHelper.showBaloon("Project path couldn't be determined. Please try again or restart the IDE.", MessageType.ERROR, project);
             return false;
         }
-
         Task.Backgroundable installDevtools = new InstallDevtoolsPlugin(project, "Installing Devtools Plugin", new File(targetPath));
-
-        URI finalTargetPath = targetPath;
         Task.Backgroundable t = new Task.Backgroundable(project, "Copying Grav SDK to Module Folder") {
 
             @Override
@@ -106,7 +80,7 @@ public class GravProjectGeneratorUtil {
 
             public void run(@NotNull ProgressIndicator progressIndicator) {
                 try {
-                    Files.walkFileTree(new File(settings.gravInstallationPath).toPath(), new CopyFileVisitor(new File(finalTargetPath).toPath()));
+                    Files.walkFileTree(new File(settings.gravInstallationPath).toPath(), new CopyFileVisitor(new File(targetPath).toPath()));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
